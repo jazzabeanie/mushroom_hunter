a54a15da2dc62926b70b2333f7ea9210cf323877
Jared Johnston <jazzab@gmail.com>
2018-11-24 11:41:57 +1000

    Add logger

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Mushroom Hunter
 ===============
 
-Call working.py, passing it a link to a json file providing [weather observations from BOM](http://www.bom.gov.au/products/IDQ60801/IDQ60801.94294.shtml). It will analyse the data and present some statistics that help to inform whether it's a good time to go foraging for mushrooms.
+Call mhunter.py, passing it a link to a json file providing [weather observations from BOM](http://www.bom.gov.au/products/IDQ60801/IDQ60801.94294.shtml). It will analyse the data and present some statistics that help to inform whether it's a good time to go foraging for mushrooms.
 
 In the future I want to have it deliver notifications when certain pre-specified conditions are met. 
--- mhunter.py
+++ mhunter.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 import argparse
 import numpy
@@ -18,6 +19,17 @@ parser.add_argument("-v",
 args = parser.parse_args()
 
 # TODO: think about security if I expose this publically. -- Bobby Tables
+
+logger = logging.getLogger('mhunter')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('log.log')
+file_handler.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 def parse_url(url):
     """removes 'shtml' and replace it with 'json'"""
